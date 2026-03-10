a8ed03ffc43c322fb5888d7f9f9f716808f752ba
yuganshjindal <yuganshjindal35@gmail.com>
2026-03-10 16:07:55 +0530

    Requirement.txt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US-Visa_Approval_Prediction",
+    version="0.0.0",
+    author="Yugansh",
+    author_email="yuganshjindal35@gmail.com",
+    packages=find_packages()
+)
